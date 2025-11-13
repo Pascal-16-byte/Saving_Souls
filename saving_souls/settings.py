ff02d6af2d7f874fcf47b028cbb15b500dddd1b6
Pascal_1606 <10cAbhineetFrancisPascal@gmail.com>
2025-11-13 20:44:29 +0530

    update changes4

--- saving_souls/settings.py
+++ saving_souls/settings.py
@@ -96,11 +96,14 @@ WSGI_APPLICATION = 'saving_souls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,  # keep connections open for performance
+        ssl_require=bool(os.environ.get("DATABASE_SSL", False))
+    )
 }
 
 
